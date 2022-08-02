66c57c2cb69e516ba2c193e5509d2d6e3cbfdf1b
Dmitriy577 <51756301+Dmitriy577@users.noreply.github.com>
2022-08-02 14:18:06 +0300

    dz - 2

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,11 +1,7 @@
 import { galleryItems } from "./gallery-items.js";
-const divGallery = document.querySelector(".gallery");
-const renderImg = galereaImag(galleryItems);
-divGallery.insertAdjacentHTML("afterbegin", renderImg)
-divGallery.addEventListener("click", selectImg);
-console.log(divGallery)
 
-function galereaImag(galleryItems) {
+const divGallery = document.querySelector(".gallery");
+function gal(galleryItems) {
   return galleryItems
     .map(({ preview, original, description }) => {
       return `
@@ -23,6 +19,7 @@ function galereaImag(galleryItems) {
   .join("");
 }
 
+divGallery.addEventListener("click", selectImg);
 function selectImg(event) {
   event.preventDefault();
   if (event.target.nodeName !== "IMG") {
@@ -30,24 +27,28 @@ function selectImg(event) {
   }
   const images = event.target.dataset.source;
   const imagesAlt = event.target.src;
-    const instance = basicLightbox.create(
-      `<img src="${images}" alt="${imagesAlt}" width="800" height="600">`,
-      {
-        onShow: () => {
-          document.addEventListener("keydown", escape);
-        },
-        onClose: () => {
-          document.removeEventListener("keydown", escape);
-        },
-      });
-    instance.show();
+  const instance = basicLightbox.create(
+    `<img src="${images}" alt="${imagesAlt}" width="800" height="600">`,
+    {
+      onShow: () => {
+        document.addEventListener("keydown", escape);
+      },
+      onClose: () => {
+        document.removeEventListener("keydown", escape);
+      },
+    });
+  instance.show();
 }
+
+// Close slider escape!!!
+const renderImg = gal(galleryItems);
+divGallery.insertAdjacentHTML("afterbegin", renderImg)
+
 function escape(event) {
   if (event.code !== "Escape") {
     return;
-}
-    
-const modal = document.querySelector(".basicLightbox");
+  }
+  const modal = document.querySelector(".basicLightbox");
   if (modal) {
     modal.classList.remove("basicLightbox--visible");
     setTimeout(() => {
